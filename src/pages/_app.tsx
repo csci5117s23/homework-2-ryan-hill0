c1006be499d4d4c6e9add45e0bfab7170bcae4fd
Ryan Hill <hill1886@umn.edu>
2023-04-12 16:36:54 -0500

    Set the HTML head correctly for all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,30 @@ import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ClerkProvider, RedirectToSignIn, SignedIn, SignedOut} from "@clerk/nextjs";
 import {useRouter} from "next/router";
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps) {
     const {pathname} = useRouter();
 
-    return <ClerkProvider {...pageProps} signInUrl='/'>
-        {pathname === '/' || pathname === '/sign-up' ? (
-            <Component {...pageProps} />
-        ) : (
-            <>
-                <SignedIn>
-                    <Component {...pageProps} />
-                </SignedIn>
-                <SignedOut>
-                    <RedirectToSignIn redirectUrl='/todos' />
-                </SignedOut>
-            </>
-        )}
-    </ClerkProvider>
+    return <>
+        <Head>
+            <title>Homework 2 To-Do List</title>
+            <meta name="description" content="A simple to-do list application"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </Head>
+        <ClerkProvider {...pageProps} signInUrl='/'>
+            {pathname === '/' || pathname === '/sign-up' ? (
+                <Component {...pageProps} />
+            ) : (
+                <>
+                    <SignedIn>
+                        <Component {...pageProps} />
+                    </SignedIn>
+                    <SignedOut>
+                        <RedirectToSignIn redirectUrl='/todos'/>
+                    </SignedOut>
+                </>
+            )}
+        </ClerkProvider>
+    </>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,17 +10,8 @@ export default function Home() {
         push('/todos').finally();
     }
 
-    return (
-        <>
-            <Head>
-                <title>Homework 2 To-Do List</title>
-                <meta name="description" content="A simple to-do list application"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            </Head>
-            <main className='w-full h-full flex flex-col gap-8 items-center justify-center'>
-                <h1>Ryan&apos;s To-Do List App</h1>
-                <SignIn />
-            </main>
-        </>
-    )
+    return <main className='w-full h-full flex flex-col gap-8 items-center justify-center'>
+        <h1>Ryan&apos;s To-Do List App</h1>
+        <SignIn/>
+    </main>;
 }
